00c5fc6ceed0637a222d90c092b557bc1699baf8
Bill Randall <william.randall@cru.org>
2015-02-03 11:36:27 -0500

    Add Ministry contract

--- app/src/main/java/com/expidev/gcmapp/db/Contract.java
+++ app/src/main/java/com/expidev/gcmapp/db/Contract.java
@@ -46,12 +46,33 @@ public class Contract {
                         SQL_COLUMN_LAST_SYNCED, SQL_PRIMARY_KEY}) + ")";
     }
 
-    public static final class AssociatedMinistry extends Base
+    public static abstract class MinistryBase extends Base
+    {
+        static final String COLUMN_MINISTRY_ID = "ministry_id";
+        static final String COLUMN_NAME = "name";
+
+        static final String SQL_COLUMN_MINISTRY_ID = COLUMN_MINISTRY_ID + " TEXT";
+        static final String SQL_COLUMN_NAME = COLUMN_NAME + " TEXT";
+        static final String SQL_PRIMARY_KEY = "PRIMARY KEY(" + COLUMN_MINISTRY_ID + ")";
+
+        static final String SQL_WHERE_PRIMARY_KEY = COLUMN_MINISTRY_ID + " = ?";
+    }
+
+    public static final class Ministry extends MinistryBase
+    {
+        public static final String TABLE_NAME = "all_ministries";
+
+        static final String[] PROJECTION_ALL = { COLUMN_MINISTRY_ID, COLUMN_NAME, COLUMN_LAST_SYNCED };
+
+        public static final String SQL_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + "(" + TextUtils
+            .join(",", new Object[] { SQL_COLUMN_MINISTRY_ID, SQL_COLUMN_NAME,
+                SQL_COLUMN_LAST_SYNCED, SQL_PRIMARY_KEY }) + ")";
+    }
+
+    public static final class AssociatedMinistry extends MinistryBase
     {
         public static final String TABLE_NAME = "associated_ministries";
 
-        static final String COLUMN_MINISTRY_ID = "ministry_id";
-        static final String COLUMN_NAME = "name";
         static final String COLUMN_MIN_CODE = "min_code";
         static final String COLUMN_HAS_SLM = "has_slm";
         static final String COLUMN_HAS_LLM = "has_llm";
@@ -64,17 +85,13 @@ public class Contract {
             COLUMN_HAS_GCM, COLUMN_PARENT_MINISTRY_ID, COLUMN_LAST_SYNCED
         };
 
-        private static final String SQL_COLUMN_MINISTRY_ID = COLUMN_MINISTRY_ID + " TEXT";
-        private static final String SQL_COLUMN_NAME = COLUMN_NAME + " TEXT";
         private static final String SQL_COLUMN_MIN_CODE = COLUMN_MIN_CODE + " TEXT";
         private static final String SQL_COLUMN_HAS_SLM = COLUMN_HAS_SLM + " INTEGER";
         private static final String SQL_COLUMN_HAS_LLM = COLUMN_HAS_LLM + " INTEGER";
         private static final String SQL_COLUMN_HAS_DS = COLUMN_HAS_DS + " INTEGER";
         private static final String SQL_COLUMN_HAS_GCM = COLUMN_HAS_GCM + " INTEGER";
         private static final String SQL_COLUMN_PARENT_MINISTRY_ID = COLUMN_PARENT_MINISTRY_ID + " TEXT";
-        private static final String SQL_PRIMARY_KEY = "PRIMARY KEY(" + COLUMN_MINISTRY_ID + ")";
 
-        static final String SQL_WHERE_PRIMARY_KEY = COLUMN_MINISTRY_ID + " = ?";
         static final String SQL_WHERE_PARENT = COLUMN_PARENT_MINISTRY_ID + " = ?";
 
         public static final String SQL_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + "(" + TextUtils
