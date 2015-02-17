61a5dd36101fbda487388704bb42e793ea466e6a
Daniel Frett <daniel.frett@ccci.org>
2015-02-16 23:18:04 -0500

    add a refresh action to the action overflow menu

--- app/src/main/java/com/expidev/gcmapp/MainActivity.java
+++ app/src/main/java/com/expidev/gcmapp/MainActivity.java
@@ -183,21 +183,16 @@ public class MainActivity extends ActionBarActivity
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
-            Intent goToSettings = new Intent(this, SettingsActivity.class);
-            startActivity(goToSettings);
-            return true;
+    public boolean onOptionsItemSelected(@NonNull final MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent goToSettings = new Intent(this, SettingsActivity.class);
+                startActivity(goToSettings);
+                return true;
+            case R.id.action_refresh:
+                MinistriesService.syncAllMinistries(this, true);
+                return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
