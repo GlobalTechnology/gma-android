a4518235ccae263ca61636b2050d9f698349a18c
Daniel Frett <daniel.frett@ccci.org>
2015-02-20 18:28:58 -0500

    update title for clusters

--- app/src/main/java/com/expidev/gcmapp/map/MarkerRender.java
+++ app/src/main/java/com/expidev/gcmapp/map/MarkerRender.java
@@ -13,12 +13,8 @@ import com.google.maps.android.clustering.view.DefaultClusterRenderer;
  * Created by matthewfrederick on 2/3/15.
  */
 public class MarkerRender extends DefaultClusterRenderer<Marker> {
-    private final Context context;
-
     public MarkerRender(Context context, GoogleMap map, ClusterManager<Marker> clusterManager) {
-        super(context.getApplicationContext(), map, clusterManager);
-
-        this.context = context;
+        super(context, map, clusterManager);
     }
 
     @Override
@@ -28,13 +24,35 @@ public class MarkerRender extends DefaultClusterRenderer<Marker> {
                 .title(item.getName());
     }
 
-    // TODO: for now disable custom icons for clusters until we figured out nuances of clustering
-//    @Override
-//    protected void onBeforeClusterRendered(Cluster<Marker> cluster, MarkerOptions markerOptions) {
-//        super.onBeforeClusterRendered(cluster, markerOptions);
-//        markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.training))
-//            .title("" + cluster.getSize() + " training activities");
-//    }
+    @Override
+    protected void onBeforeClusterRendered(Cluster<Marker> cluster, MarkerOptions markerOptions) {
+        super.onBeforeClusterRendered(cluster, markerOptions);
+        int churches = 0;
+        int trainings = 0;
+        for (final Marker marker : cluster.getItems()) {
+            if(marker instanceof ChurchMarker) {
+                churches++;
+            } else if(marker instanceof TrainingMarker) {
+                trainings++;
+            }
+        }
+
+        // generate the title for this cluster
+        final StringBuilder title = new StringBuilder();
+        if(churches > 0) {
+            title.append(churches).append(" Churches");
+        }
+        if(trainings > 0) {
+            if(title.length() > 0) {
+                title.append(" and ");
+            }
+            title.append(trainings).append(" training activities");
+        }
+        markerOptions.title(title.toString());
+
+        // TODO: for now disable custom icons for clusters until we figured out nuances of clustering
+//        markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.training));
+    }
 
     @Override
     protected boolean shouldRenderAsCluster(Cluster<Marker> cluster) {
