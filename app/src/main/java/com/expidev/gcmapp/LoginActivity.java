0a9036c8e5f6c9932e96b262b831f457796bd774
samija <jasmen.com>
2015-03-10 16:49:56 +0200

    Some fixes

--- app/src/main/java/com/expidev/gcmapp/LoginActivity.java
+++ app/src/main/java/com/expidev/gcmapp/LoginActivity.java
@@ -4,24 +4,22 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
-import com.google.android.gms.common.images.ImageManager;
 
 /**
  * Created by Sami on 3/9/15.
  */
 public class LoginActivity extends Activity {
 
-
+    ImageButton btsignin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
-        ImageButton btsign_in = (ImageButton) findViewById(R.id.btsing_in);
+       btsignin = (ImageButton) findViewById(R.id.btsing_in);
 
-        btsign_in.setOnClickListener(new View.OnClickListener() {
+        btsignin.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
