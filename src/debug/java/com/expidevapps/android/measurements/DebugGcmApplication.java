1bb0e4eec535da84f41409c6015f4a50c129bb80
Daniel Frett <daniel.frett@ccci.org>
2015-10-01 16:46:01 -0400

    enable stetho js console
    this requires multidex to work, but is only enabled for debug builds

--- src/debug/java/com/expidevapps/android/measurements/DebugGcmApplication.java
+++ src/debug/java/com/expidevapps/android/measurements/DebugGcmApplication.java
@@ -1,8 +1,17 @@
 package com.expidevapps.android.measurements;
 
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
 import com.facebook.stetho.Stetho;
 
 public class DebugGcmApplication extends GcmApplication {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
